d7adcfe663524be268c7718ea010e8e17d9102d1
Affan Ahsanul Habib <affanahsan02@gmail.com>
2020-04-12 19:34:24 +0700

    API baru

--- connection/conect.js
+++ connection/conect.js
@@ -1,13 +1,12 @@
-var sql = require("mssql");
-var connect = function()
-{
-    var conn = new sql.ConnectionPool({
-        user: 'sa',
-        password: 'SaSa1212',
-        server: '10.199.13.253',
-        database: 'nrp05111740000091'
-    });
-    console.log(conn)
-    return conn;
-};
-module.exports = connect;
+const dbconf = {
+    user: 'sa',
+    password: 'SaSa1212',
+    server: '10.199.13.253',
+    database: 'nrp05111740000091',
+    "options": {
+     "encrypt": true,
+     "enableArithAbort": true
+     }
+ };
+
+module.exports = dbconf;
